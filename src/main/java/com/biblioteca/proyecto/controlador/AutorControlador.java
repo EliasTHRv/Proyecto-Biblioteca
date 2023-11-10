a1963aedeafd26107b9c8a55e7868e816ca43708
Elias Barreto <eliasgobarreto@gmail.com>
2023-11-09 22:01:30 -0300

    Primer commit

---
+++ src/main/java/com/biblioteca/proyecto/controlador/AutorControlador.java
@@ -0,0 +1,70 @@
+package com.biblioteca.proyecto.controlador;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.biblioteca.proyecto.entidades.Autor;
+import com.biblioteca.proyecto.exepciones.MiException;
+import com.biblioteca.proyecto.servicios.AutorServicio;
+
+@Controller
+@RequestMapping("/autor") // localhost:8080/autor
+public class AutorControlador {
+
+    @Autowired
+    private AutorServicio autorServicio;
+
+    @GetMapping("/registrar") // localhost:8080/autor/registrar
+    public String registar() {
+        return "autor_form.html";
+    }
+
+    @PostMapping("/registro")
+    public String registro(@RequestParam String nombre, ModelMap modelo) {
+
+        try {
+            autorServicio.crearAutor(nombre);
+            modelo.put("exito", "El autor se cargo con exito.");
+
+        } catch (MiException ex) {
+            Logger.getLogger(EditorialControlador.class.getName()).log(Level.SEVERE, null, ex);
+            modelo.put("error", ex.getMessage());
+
+            return "autor_form.html";
+
+        }
+
+        // System.out.println("Nombre: " + nombre);
+        return "index.html";
+    }
+
+    @GetMapping("/lista") // localhost:8080/autor/lista
+    public String listar(ModelMap modelo) {
+
+        List<Autor> autores = autorServicio.listarAutores();
+
+        modelo.addAttribute("autores", autores);
+
+        return "autor_list.html";
+
+    }
+
+    @GetMapping("/modificar/{id}")
+    public String modificar(@PathVariable String id, ModelMap modelo){
+       
+        modelo.put("autor", autorServicio.getOne(id));
+
+        return "autor_modificar.html";
+    }
+
+}
---
+++ src/main/java/com/biblioteca/proyecto/servicios/AutorServicio.java
@@ -0,0 +1,73 @@
+package com.biblioteca.proyecto.servicios;
+
+import com.biblioteca.proyecto.entidades.Autor;
+import com.biblioteca.proyecto.exepciones.MiException;
+import com.biblioteca.proyecto.repositorios.AutorRepositorio;
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/**
+ *
+ * @author benit
+ */
+@Service
+public class AutorServicio {
+
+    @Autowired
+    private AutorRepositorio autorRepositorio;
+
+    @Transactional
+    public void crearAutor(@RequestParam String nombre) throws MiException {
+ 
+        validar(nombre);
+
+        Autor autor = new Autor();
+        autor.setNombre(nombre);
+
+        autorRepositorio.save(autor);
+    }
+
+    public List<Autor> listarAutores() {
+        
+        List<Autor> autores = new ArrayList<Autor>();
+        autores = autorRepositorio.findAll();
+
+        return autores;
+    }
+
+    public void modificarAutor(String id, String nombre) throws MiException {
+
+        validar(nombre);
+
+        Optional<Autor> respuesta = autorRepositorio.findById(id);
+
+        if (respuesta.isPresent()) {
+            Autor autor = respuesta.get();
+
+            autor.setNombre(nombre);
+
+            autorRepositorio.save(autor);
+
+        }
+    }
+
+    public Autor getOne(String id){
+        return autorRepositorio.findById(id).get();
+    }
+
+    private void validar(String nombre) throws MiException {
+
+        if (nombre == null || nombre.isEmpty()) {
+            throw new MiException("El Nombre no puede ser nulo o estar vacio");
+
+        }
+
+    }
+
+}
